ae2e1a8a2a2f1d4a50dc0ea4ff256ceba671918a
Vitalii Nesterenko <nesterenkov90@gmail.com>
2023-01-07 21:48:09 +0200

    solved 84

---
+++ LeetCode/84_Largest_Rectangle_in_Histogram.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define gc getchar_unlocked
+#define fo(i, n) for (i = 0; i < n; i++)
+#define Fo(i, k, n) for (i = k; k < n ? i < n : i > n; k < n ? i += 1 : i -= 1)
+#define ll long long
+#define deb(x) cout << #x << "=" << x << endl
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, false, sizeof(x))
+#define sortall(x) sort(all(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define PI 3.1415926535897932384626
+#define mod 1000000007
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        stack<pair<int,int>> s; // (index, height)
+        int m=INT_MIN;
+        for(int i=0;i<heights.size();i++){
+            if(s.empty()){
+                s.push(make_pair(i,heights[i]));
+            }else{
+                auto prev = s.top();
+                if(prev.second > heights[i]){
+                    while(true){
+                        s.pop();
+                        int pq=(i-prev.first)*prev.second;
+                        m=max(m,pq); // calc max square only when stack is popped
+                        if(!s.empty() && s.top().second > heights[i]){
+                            prev=s.top();
+                        }else{
+                            break;
+                        }
+                    }
+                    s.push(make_pair(prev.first, heights[i]));
+                }else{
+                    s.push(make_pair(i,heights[i]));
+                }
+            }
+        }
+        while(!s.empty()){
+            auto t=s.top();
+            int pq=(heights.size()-t.first)*t.second;
+            m=max(m,pq);
+            s.pop();
+        }
+        return m;
+    }
+};
+
+void solve() {
+    vi h = { 2,1,5,6,2,3 };
+    Solution s;
+    cout << s.largestRectangleArea(h);
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    solve();
+    return 0;
+}
